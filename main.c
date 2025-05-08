18b9526b9546e6a4495281268d0e49024c4f7d88
Charalampos Mainas <cmainas@nubificus.co.uk>
2025-05-08 08:56:41 +0000

    Add inital version of urunit
    Add the source code of the first version of urunit, which is able to
    spawn a new app and wait till the app (and its children) exit to
    poweroff.
    
    Also a first attempt for handling cli arguments for app with spaces.
    
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

---
+++ main.c
@@ -0,0 +1,290 @@
+// Copyright (c) 2023-2025, Nubificus LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Parts of the following code are taken from
+// https://github.com/krallin/tini/tree/master
+// which comes with the The MIT License (MIT)
+// In particular:
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Thomas Orozco <thomas@orozco.fr>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// For more information, please check https://github.com/krallin/tini/blob/master/LICENSE
+
+#include <linux/reboot.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/prctl.h>
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define STATUS_MAX 255
+#define STATUS_MIN 0
+
+int isolate_child(void) {
+	// Put the child into a new process group.
+	if (setpgid(0, 0) < 0) {
+		perror("setpgid");
+		return 1;
+	}
+
+	// If there is a tty, allocate it to this new process group. We
+	// can do this in the child process because we're blocking
+	// SIGTTIN / SIGTTOU.
+
+	// Doing it in the child process avoids a race condition scenario
+	// if Tini is calling Tini (in which case the grandparent may make the
+	// parent the foreground process group, and the actual child ends up...
+	// in the background!)
+	if (tcsetpgrp(STDIN_FILENO, getpgrp())) {
+		if (errno != ENOTTY && errno != ENXIO) {
+			perror("tcsetpgrp");
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int spawn_app(int argc, char *argv[], pid_t *child_pid) {
+	int i = 0;
+	pid_t pid;
+	char *new_argv[128];
+	// The arguments of the app are the same as the ones for urunit, but
+	// removing the urunit argv[0]. Therefore:
+	int new_argc = 0;
+
+	for (i = 1; i < argc; i++) {
+		char *tmp_arg = argv[i];
+
+		if (tmp_arg[0] == '\'') {
+			// This arg (and everything until we encounter a ')
+			// is part of the same argument
+			int j = 0;
+			char buffer[1024] = {0};
+
+			strcat(buffer, tmp_arg + 1); // skip '
+			for (j = i + 1; j < argc; j++) {
+				char *next_arg = argv[j];
+				size_t arg_len = strlen(next_arg);
+				uint32_t last_char = 0;
+				uint8_t should_break = 0;
+				if (arg_len == 0) {
+					continue;
+				}
+				last_char = arg_len - 1;
+				if (last_char == 0) {
+					if (next_arg[last_char] == '\'') {
+						should_break = 1;
+						// Remove '
+						next_arg[last_char] = '\0';
+					}
+				} else {
+					if (next_arg[last_char] == '\'' && next_arg[last_char - 1] != '\'' ) {
+						should_break = 1;
+						// Remove '
+						next_arg[last_char] = '\0';
+					}
+				}
+				strcat(buffer, " ");
+				strcat(buffer, next_arg);
+				if (should_break) {
+					break;
+				}
+			}
+			new_argv[new_argc++] = strdup(buffer);
+			break;
+		} else if (tmp_arg[0] == '"') {
+			// This arg (and everything until we encounter a ")
+			// is part of the same argument
+			int j = 0;
+			char buffer[1024] = {0};
+
+			strcat(buffer, tmp_arg + 1); // skip "
+			for (j = i + 1; j < argc; j++) {
+				char *next_arg = argv[j];
+				size_t arg_len = strlen(next_arg);
+				uint32_t last_char = 0;
+				uint8_t should_break = 0;
+				if (arg_len == 0) {
+					continue;
+				}
+				last_char = arg_len - 1;
+				if (last_char == 0) {
+					if (next_arg[last_char] == '"') {
+						should_break = 1;
+						// Remove '
+						next_arg[last_char] = '\0';
+					}
+				} else {
+					if (next_arg[last_char] == '"' && next_arg[last_char - 1] != '"' ) {
+						should_break = 1;
+						// Remove '
+						next_arg[last_char] = '\0';
+					}
+				}
+				strcat(buffer, " ");
+				strcat(buffer, next_arg);
+				if (should_break) {
+					break;
+				}
+			}
+			new_argv[new_argc++] = strdup(buffer);
+			break;
+		} else {
+			new_argv[new_argc++] = argv[i];
+		}
+	}
+	new_argv[new_argc] = NULL;
+
+	pid = fork();
+	if (pid < 0) {
+		perror("fork");
+		return 1;
+	} else if (pid == 0) {
+		int status = 1;
+
+		// Put the child in a process group and
+		// make it the foreground process if there is a tty.
+		if (isolate_child()) {
+			return 1;
+		}
+
+		execvp(new_argv[0], new_argv);
+
+		// execvp will only return on an error so make sure that we check the errno
+		// and exit with the correct return status for the error that we encountered
+		// See: http://www.tldp.org/LDP/abs/html/exitcodes.html#EXITCODESREF
+		switch (errno) {
+		case ENOENT:
+			status = 127;
+			break;
+		case EACCES:
+			status = 126;
+			break;
+		}
+		perror("execv failed");
+		return status;
+	} else {
+		*child_pid = pid;
+		return 0;
+	}
+
+	return 1;
+}
+
+int reap(const pid_t child_pid, int *child_exitcode_ptr) {
+	pid_t reaped_pid = 0;
+	int reaped_status = 0;
+
+	while (1) {
+		reaped_pid = waitpid(-1, &reaped_status, WNOHANG);
+
+		switch (reaped_pid) {
+		case -1:
+			if (errno == ECHILD) {
+				break;
+			}
+			perror("reaping");
+			return 1;
+
+		case 0:
+			break;
+		default:
+			// A child was reaped. Check whether it's the app.
+			// If it is, then set the exit_code,
+			if (reaped_pid == child_pid) {
+				if (WIFEXITED(reaped_status)) {
+					// The app exited normally
+					*child_exitcode_ptr = WEXITSTATUS(reaped_status);
+				} else if (WIFSIGNALED(reaped_status)) {
+					/* The app was terminated. Emulate what sh / bash
+					 * would do, which is to return
+					 * 128 + signal number.
+					 */
+					*child_exitcode_ptr = 128 + WTERMSIG(reaped_status);
+				} else {
+					return 1;
+				}
+
+				// Be safe, ensure the status code is indeed between 0 and 255.
+				*child_exitcode_ptr = *child_exitcode_ptr % (STATUS_MAX - STATUS_MIN + 1);
+
+			}
+			continue;
+		}
+		/* If we make it here, that's because we did not continue in the switch case. */
+		break;
+	}
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	pid_t app_pid;
+	int ret = 0;
+	int app_exitcode = -1;
+
+	ret = prctl(PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0);
+	if (ret < 0) {
+		perror("Become subreaper");
+		return 1;
+	}
+
+	ret = spawn_app(argc, argv, &app_pid);
+	if (ret) {
+		fprintf(stderr, "Could not spawn app\n");
+		return ret;
+	}
+
+	while (1) {
+		ret = reap(app_pid, &app_exitcode);
+		if (ret) {
+			fprintf(stderr, "Error while reaping %d", ret);
+			break;
+		}
+
+		if (app_exitcode != -1) {
+			break;
+		}
+	}
+
+	sync();
+	syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2,
+		LINUX_REBOOT_CMD_POWER_OFF, NULL);
+}
